f0eb0e8ce2d3306e97911285f36a3ce4e273d77b
jackAzadian <jack.azadian96@gmail.com>
2019-02-11 17:57:11 -0500

    first prototype

---
+++ puzzle game/Assets/Floor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+    public GameObject[] adjacents = new GameObject[4];
+
+    public bool active = false;
+
+	// Use this for initialization
+	void Start () {
+
+        Vector3 pos = transform.position;
+        adjacents[0] = FindAt(new Vector3(pos.x + 2, pos.y, pos.z));
+        adjacents[1] = FindAt(new Vector3(pos.x, pos.y, pos.z + 2));
+        adjacents[2] = FindAt(new Vector3(pos.x - 2, pos.y, pos.z));
+        adjacents[3] = FindAt(new Vector3(pos.x, pos.y, pos.z - 2));
+
+        if(gameObject.tag == "Start")
+        {
+            active = true;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private GameObject FindAt(Vector3 pos)
+    {
+        var cols = Physics.OverlapSphere(pos, 0.1f);
+
+        float dist = Mathf.Infinity;
+
+        GameObject nearest = null;
+
+        foreach(Collider element in cols)
+        {
+            var d = Vector3.Distance(pos, element.transform.position);
+            if (d < dist)
+            { // if closer...
+                dist = d; // save its distance... 
+                nearest = element.gameObject; // and its gameObject
+            }
+        }
+
+        return nearest;
+    }
+}
---
+++ puzzle game/Assets/Player.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonoGraph;
+using MonoGraph.Algorithms;
+
+public class Player : MonoBehaviour {
+
+    CharacterController cc;
+
+    public float speed = 10f;
+
+    //gravity stuff
+    float ySpeed = 0;
+    float gravity = -15f;
+
+    public Transform fpsCamera;
+
+    float pitch = 0f;
+    AdjacencyListGraph<string, Edge<string>> floorGraph;
+
+    GameObject lastHit = null;
+
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        cc = GetComponent<CharacterController>();
+
+        floorGraph = new AdjacencyListGraph<string, Edge<string>>();
+        floorGraph.AddVertex("Start");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float xInput = Input.GetAxis("Horizontal") * speed;
+        float zInput = Input.GetAxis("Vertical") * speed;
+
+        Vector3 move = new Vector3(xInput, 0, zInput);
+        move = Vector3.ClampMagnitude(move, speed);
+        move = transform.TransformVector(move);
+
+        if(cc.isGrounded)
+        {
+            if(Input.GetButtonDown("Jump"))
+            {
+                ySpeed = 15f;
+            }
+            else
+            {
+                ySpeed = gravity*Time.deltaTime;
+            }
+        }
+        else
+        {
+            ySpeed += gravity * Time.deltaTime;
+        }
+
+        cc.Move((move + new Vector3(0,ySpeed,0)) * Time.deltaTime);
+
+        float xMouse = Input.GetAxis("Mouse X")* 5f;
+
+        transform.Rotate(0, xMouse, 0);
+
+        pitch -= Input.GetAxis("Mouse Y") * 5f;
+        pitch = Mathf.Clamp(pitch,30f,60f);
+
+        Quaternion camRotation = Quaternion.Euler(pitch, 0, 0);
+
+        fpsCamera.localRotation = camRotation;
+
+        checkWin();
+	}
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        GameObject body = hit.gameObject;
+        if(lastHit == body)
+        {
+            return;
+        }
+        lastHit = body;
+        if (body.tag == "Floor")
+        {
+            Floor floor = body.GetComponent<Floor>();
+            bool connectedToEnd = false;
+            if (floor.active == false)
+                foreach (GameObject element in floor.adjacents)
+                {
+                    if (element != null)
+                    {
+                        if(element.tag == "End")
+                        {
+                            connectedToEnd = true;
+                        }
+                        Floor adjacent = element.GetComponent<Floor>();
+                        if (adjacent.active == true)
+                        {
+                            floorGraph.AddVertex(body.name);
+                            floorGraph.AddBidirectionalEdge(new Edge<string>(body.name, element.name));
+                            floor.active = true;
+                            Material mat = body.GetComponent<Renderer>().material;
+                            mat.color = new Color(255, 255, 0);
+                            if(connectedToEnd)
+                            {
+                                floorGraph.AddVertex("End");
+                                floorGraph.AddBidirectionalEdge(new Edge<string>(body.name, "End"));
+                            }
+                        }
+                    }
+                }
+
+        }
+
+        
+    }
+
+    void checkWin()
+    {
+        if (floorGraph.ContainsVertex("End"))
+            Debug.Log("WIN");
+    }
+
+}
